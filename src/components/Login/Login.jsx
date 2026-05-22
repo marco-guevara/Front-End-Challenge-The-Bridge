6b2b43de9bd95530cc615763215a6d4f76e598b9
mattos-rafael <rafa.rafa.rafa07@gmail.com>
2026-05-22 11:35:30 +0200

    add authContext, changed api calls to functions in authContext

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { AuthShell, StatusMessage } from "../Auth/AuthShell";
-import api from "../../services/api";
+
 import styles from "./Login.module.css";
+import useAuth from "../../context/useAuth";
 
 function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const {error, login, setError} = useAuth()
 
   const handleChange = (event) => {
     setFormData({
@@ -22,11 +24,12 @@ function Login() {
     event.preventDefault();
 
     try {
-      const response = await api.post("/auth/login", formData);
-      setSuccess(response.data.message);
+      const response = await login(formData);
+      setSuccess(response.message);
       setError("");
     } catch (err) {
-      setError(err.response?.data?.message || "Error al iniciar sesion");
+      console.log(err)
+      // setError(err.response?.message || "Error al iniciar sesion");
       setSuccess("");
     }
   };
